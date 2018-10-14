655b469bace2e84ee5906572b17c8bcd5c86ab90
timcrose <timcrose09@gmail.com>
2018-10-14 04:12:30 +0000

    added ability to find nth from the end of the str

--- str_utils.py
+++ str_utils.py
@@ -1,6 +1,13 @@
-
-def find_nth_occurrence_in_str(input_str, search_str, n):
-    parts= input_str.split(search_str, n+1)
-    if len(parts)<=n+1:
-        return -1
-    return len(input_str)-len(parts[-1])-len(search_str)
+def find_nth_occurrence_in_str(input_str, search_str, n, reverse=False, overlapping=0):
+    if reverse:
+        match = input_str.rfind(search_str)
+        while match >= 0 and n > 0:
+            match = input_str.rfind(search_str, 0, match + (len(search_str) - 1) * overlapping)
+            n -= 1
+        return match
+    else:
+        match = input_str.find(search_str)
+        while match >= 0 and n > 0:
+            match = input_str.find(search_str, match + len(search_str) ** overlapping)
+            n -= 1
+        return match
