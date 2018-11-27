38b270b7d1e569d16b05fbdc89ed4d1633391dc4
timcrose <trose@andrew.cmu.edu>
2018-11-26 22:02:34 -0500

    indices() added to list_utils

--- list_utils.py
+++ list_utils.py
@@ -18,3 +18,17 @@ def concat_str_list(str_list, delimiter=' '):
         concat_str += elem + delimiter
     return concat_str
 
+def indices(lst, val):
+    '''
+    lst: list
+        list to search
+    val: *
+        value to search the list for
+
+    return: list
+        All indices matching val. If there are no indices
+        in lst matching val, return empty list
+    Purpose: output indices where the val is found in lst. If val is
+        not found in lst, output empty list
+    '''
+    return [item for i, item in enumerate(lst) if item is val]
