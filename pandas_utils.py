ccb36aec8b298d5793d65c902c915e61450fa68c
timcrose <timcrose09@gmail.com>
2019-05-20 20:22:00 -0400

    updated docs for insert_df_into_df

--- pandas_utils.py
+++ pandas_utils.py
@@ -4,16 +4,27 @@ import numpy as np
 
 def insert_df_into_df(inserted_df, df):
     '''
-    inserted_df: pd.DataFrame
+    inserted_df: pd.DataFrame or pd.Series
         Must have the same columns as df and compatible index types. e.g.
         line = DataFrame({"onset": 30.0, "length": 1.3}, index=[2.5])
-    df: pd.DataFrame
-        Host df
+    df: pd.DataFrame or pd.Series
+        Host df or series
     
-    return: pd.DataFrame
+    return: pd.DataFrame or pd.Series
         Now the rows of inserted_df are incorporated into df with the indices sorted
         
     Purpose: Insert the rows of one df into another and then sort the rows by index
+
+    Notes: It will break ties in index value by putting the inserted values after the
+        original ones when they were in their original position. e.g.
+        df = pd.DataFrame({"onset": [30.0,20.0], "length": [1.3,1.2]}, index=range(2))
+        df_inserted = pd.DataFrame({"onset": [40.0,50.0], "length": [1.4,1.5]}, index=[0,1])
+        print(pandas_utils.insert_df_into_df(df_inserted, df))
+        ->     length  onset
+           0     1.3   30.0
+           1     1.4   40.0
+           2     1.2   20.0
+           3     1.5   50.0
     '''
     df = df.append(inserted_df, ignore_index=False)
     df = df.sort_index().reset_index(drop=True)
