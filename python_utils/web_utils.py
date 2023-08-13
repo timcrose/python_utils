725dba564c02336945c3a53f282c58a003fac528
timcrose <timcrose09@gmail.com>
2023-08-13 00:03:15 -0400

    more robust check whether online or not

--- python_utils/web_utils.py
+++ python_utils/web_utils.py
@@ -5,6 +5,7 @@ Created on Tue Feb  6 19:26:59 2018
 """
 
 from subprocess import Popen, PIPE, STDOUT
+import requests
 
 def connected_to_internet(hostname):
     toping = Popen(['ping', hostname, '-c', '1'], stdin=PIPE, stdout=PIPE, stderr=STDOUT)
@@ -21,3 +22,14 @@ def wait_until_connected_to_internet():
     hostname = 'zinc.materials.cmu.edu'
     while not connected_to_internet(hostname):
         pass
+
+
+def online():
+    url = 'http://www.google.com/'
+    timeout = 5
+    try:
+        request = requests.get(url, timeout=timeout)
+        return True
+    except (requests.ConnectionError, requests.Timeout) as exception:
+        return False
+    return False
