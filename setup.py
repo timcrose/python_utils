fb41f61b1b0844ff4a5e64f0caac3ee858169087
timcrose <timcrose09@gmail.com>
2024-08-23 18:19:31 -0400

    included dill in the automatic dependency installation

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name = 'python_utils',
       author = 'T. Rose',
       url = 'https://github.com/timcrose/python_utils',
       packages = find_packages(),
-      install_requires = ['numpy', 'pandas']
+      install_requires = ['numpy', 'pandas', 'dill']
      )
